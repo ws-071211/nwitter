9ebd616fa33cd1be5077a610d5cdc2b026612ce4
민우석 <s23044@gsm.hs.kr>
2024-02-15 23:56:39 +0900

    2-3 add firebase

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCKBzBlISNkXiKpHVJnwuRCDslmS33mRQ",
+  authDomain: "nwitter-reload-38980.firebaseapp.com",
+  projectId: "nwitter-reload-38980",
+  storageBucket: "nwitter-reload-38980.appspot.com",
+  messagingSenderId: "96893739282",
+  appId: "1:96893739282:web:984d139fbb6739cc3c93cb"
+};
+
+const app = initializeApp(firebaseConfig);
