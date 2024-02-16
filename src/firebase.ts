4f0c662ea6ca380bb50e0dcdb0f37c3e3719e3e0
민우석 <s23044@gsm.hs.kr>
2024-02-16 23:48:01 +0900

    3-0 set

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCKBzBlISNkXiKpHVJnwuRCDslmS33mRQ",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
