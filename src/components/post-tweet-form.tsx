b8b12be392794307afbc52d7150a0a474807538c
민우석 <s23044@gsm.hs.kr>
2024-02-28 23:46:09 +0900

    4-6 delete tweet

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -84,10 +84,7 @@ export default function PostTweetForm() {
         userId: user.uid,
       });
       if (file) {
-        const locationRef = ref(
-          storage,
-          `tweets/${user.uid}-${user.displayName}/${doc.id}`
-        );
+        const locationRef = ref(storage, `tweets/${user.uid}/${doc.id}`);
         const result = await uploadBytes(locationRef, file);
         const url = await getDownloadURL(result.ref);
         await updateDoc(doc, {
